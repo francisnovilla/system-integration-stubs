a8f3f33b58434eb55caf88e2c745e6d0aff3847b
Francis Novilla <fanovilla@gmail.com>
2018-05-04 21:40:55 +1200

    add license headers

--- src/main/groovy/scripts/StubsRunner.groovy
+++ src/main/groovy/scripts/StubsRunner.groovy
@@ -1,22 +1,24 @@
+/*
+ * Copyright (C) 2018 Francis Novilla
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package scripts
 
 import org.ancillaryarm.sis.stubsmanagement.StubsMain
 
-print "!"
-
-///**
-// * Created by francis on 1/01/15.
-// */
-
-http://camel.apache.org/running-camel-standalone-and-have-it-keep-running.html
-// create a Main instance
+// http://camel.apache.org/running-camel-standalone-and-have-it-keep-running.html
 main = new StubsMain()
-// enable hangup support so you can press ctrl + c to terminate the JVM
-main.enableHangupSupport();
-// bind MyBean into the registery
-//main.bind("foo", new MyBean());
-//// add routes
-//main.addRouteBuilder(new MyRouteBuilder());
 
 // run until you terminate the JVM
 System.out.println("Starting Camel. Use ctrl + c to terminate the JVM.\n");
